9cc3a87a1efcd1a0e5c6565bbbc06024ec1620f2
markuhei <markuhei@github.uio.no>
2019-08-24 16:43:35 +0200

    La til oppgave 7

--- 1. Uke/Oppgaver/oppgave4.py
+++ 1. Uke/Oppgaver/oppgave4.py
@@ -1,13 +1,17 @@
 '''Hva blir variablen a til i de ulike
 uttrykkene? Skriv svaret som kommentarer.'''
 
-a = 4+5
+a = 4 + 5
 print("4+5 gir a verdien", a)
-a = 6-2
+
+a = 6 - 2
 print("6-2 gir a verdien", a)
-a = 4*2
+
+a = 4 * 2
 print("4*2 gir a verdien", a)
-a = 11/2
+
+a = 11 / 2
 print("11/2 gir a verdien", a)
-a = 11//2
+
+a = 11 // 2
 print("11//2 gir a verdien", a)
---
+++ 1. Uke/Oppgaver/oppgave7.py
@@ -0,0 +1,9 @@
+'''UTFORDRING!'''
+
+a = 4
+b = 6
+
+if a > b:
+    print(a, "er storre enn", b)
+else:
+    print(b, "er storre enn", a)
