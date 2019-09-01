e91b1f56beb2b424b3190fae7adcda307c494b1d
markuhei <markuhei@github.uio.no>
2019-09-01 09:44:50 +0200

    Fikset opp i feilmeldinger.py

--- 2. Uke/feilmeldinger.py
+++ 2. Uke/feilmeldinger.py
@@ -1,18 +1,18 @@
 
 # print(a) # Feil
-# print("a") #Løsning
+print("a") #Løsning
 
 # print(a # Feil
-# print("a")
+print("a")
 
 # "streng" < 5 # Feil
-# print("a" + 1)
+print("a" + 1)
 
-# "streng" < str(5) #Løsning
-# print("a" + str(1))
+"streng" < str(5) #Løsning
+print("a" + str(1))
 
-# int("a") # Feil
-# "Ikke konverter bokstaver til tall"
+int("a") # Feil
+"Ikke konverter bokstaver til tall"
 
-# 10/0 #Feil
-# "Ikke del på 0"
+10/0 #Feil
+"Bare ikke del på 0"
