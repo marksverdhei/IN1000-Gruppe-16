13a9996c1324e419395d88d21839e1728b43a9eb
markuhei <markuhei@github.uio.no>
2019-10-07 20:23:03 +0200

    La til presentasjon

---
+++ 7. Uke/kode/test.py
@@ -0,0 +1,11 @@
+from person import Person
+
+person1 = Person("Magnus", 22)
+
+person2 = Person("Nicholas", 30)
+
+person1.skriv_navn()
+person1.skriv_alder()
+
+person2.skriv_navn()
+person2.skriv_alder()
